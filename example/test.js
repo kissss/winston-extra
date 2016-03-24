c41fa83344ed6ba92e50ab28a7ee6b0b74abad3d
XiaoLei <Mr_death@163.com>
2016-03-25 00:28:14 +0800

    add color

--- example/test.js
+++ example/test.js
@@ -1,12 +1,15 @@
 var winston = require('winston');
 var ExtraConsole = require('../lib');
-
+var moment =require('moment');
 
 var logger = new (winston.Logger)({
     transports: [
         new (ExtraConsole)({
+            timestamp: function() {
+                return moment(Date.now()).format('YYYY-MM-DD HH:mm:ss.SSS').grey;
+            },
             level: 'info',
-            colorize: 'all',
+            colorize: true,
             prettyPrint: true,
             handleExceptions: true
         })
